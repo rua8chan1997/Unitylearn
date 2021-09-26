3536967d129a1a417a94ae5956d71b8a4e63709c
rua8chan1997 <rua8chan1997@gmail.com>
2021-09-26 11:12:25 +0700

    add project Challeger2

---
+++ UnityProject/Challenge2 - Play Fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private bool canSpawn = true;
+    private float spawnInterval = 0.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && canSpawn)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            canSpawn = false;
+            Invoke("EnableSpawn", spawnInterval);
+        }
+    }
+
+    void EnableSpawn()
+    {
+        canSpawn = true;
+    }
+}
