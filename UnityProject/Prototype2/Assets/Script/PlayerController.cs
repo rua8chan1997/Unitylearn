8389013ac00e84cfe50860aa3fbaf924ab5ca23b
rua8chan1997 <rua8chan1997@gmail.com>
2021-09-26 20:23:20 +0700

    modifile Challenge2

--- UnityProject/Prototype2/Assets/Script/PlayerController.cs
+++ UnityProject/Prototype2/Assets/Script/PlayerController.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 10f;
     public float xRange = 10f;
+    public float zRangeTop = 14f;
+    public float zRangeBottom = 0f;
+
+    public Transform projectileSpawnPoint;
 
     public GameObject projectilePrefab;
     // Start is called before the first frame update
@@ -27,16 +32,26 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+        if(transform.position.z > zRangeTop)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRangeTop);
+        }
+        if (transform.position.z < zRangeBottom)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRangeBottom);
+        }
 
         //shoot banana
         if (Input.GetKeyDown(KeyCode.Space))
         {
             //launch a projectile  from player
-            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+            Instantiate(projectilePrefab, projectileSpawnPoint.position, projectilePrefab.transform.rotation);
         }
 
         //movement player
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * verticalInput * speed);
     }
 }
--- UnityProject/Prototype2/Assets/Script/SpawnManager.cs
+++ UnityProject/Prototype2/Assets/Script/SpawnManager.cs
@@ -5,25 +5,29 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    // Spawn top animal
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    // Spawn left, right animal
+    public float sideSpawnMinZ;
+    public float sideSpawnMaxZ;
+    public float sideSpawnX;
 
+    // Delay spawn
     private float startDelay = 2f;
-    private float spawnInterval = 1.5f;
-    /*  public int animalIndex;*/
+    /*private float spawnInterval = 1.5f;*/
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+        InvokeRepeating("SpawnRandomAnimal", startDelay, Random.Range(2,4));
+        InvokeRepeating("SpawnLeftAnimal", startDelay, Random.Range(2, 4));
+        InvokeRepeating("SpawnRightAnimal", startDelay, Random.Range(2, 4));
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*if (Input.GetKeyDown(KeyCode.S))
-        {
-            SpawnRandomAnimal();
-        }*/
+      
     }
 
     void SpawnRandomAnimal()
@@ -32,4 +36,21 @@ public class SpawnManager : MonoBehaviour
             int animalIndex = Random.Range(0, animalPrefabs.Length);
             Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
+
+    void SpawnLeftAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(-sideSpawnX, 0, Random.Range(sideSpawnMinZ,sideSpawnMaxZ));
+        Vector3 rotation = new Vector3(0, 90, 0);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, Quaternion.Euler(rotation));
+    }
+
+    void SpawnRightAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(sideSpawnX, 0, Random.Range(sideSpawnMinZ,sideSpawnMaxZ));
+        Vector3 rotation = new Vector3(0, -90, 0);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, Quaternion.Euler(rotation));
+    }
+
 }
